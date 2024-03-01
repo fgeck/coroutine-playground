5ab20ef1d1d66678c971a4d506c2b5afa45eba6d
Florian Geckeler <f.geckeler@sap.com>
2024-03-01 15:08:39 +0100

    play with channel capacity and amount of consumers

--- app/src/main/kotlin/playground/channels/Channel.kt
+++ app/src/main/kotlin/playground/channels/Channel.kt
@@ -1,5 +1,6 @@
 package playground.channels
 
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
@@ -10,7 +11,7 @@ import mu.KotlinLogging
 
 class Channel {
     private val logger = KotlinLogging.logger {}
-    private val channel = Channel<Int>()
+    private val channel = Channel<Int>(5)
     private val consumers = mutableListOf<Job>()
     private val someNumbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
@@ -20,15 +21,17 @@ class Channel {
                 logger.info { "Sending $x" }
                 channel.send(x)
             }
+            logger.info { "-------- Done Sending All --------" }
             channel.close()
         }
     }
 
     fun startConsumer(scope: CoroutineScope) {
-        repeat(3) {
-            val consumer = scope.launch {
+        repeat(5) {
+            val consumer = scope.launch(CoroutineName("Consumer #${it + 1}")) {
+                delay(5000)
                 for (y in channel) {
-                    logger.info { "Received $y" }
+                    logger.info { "Coroutine: ${this.coroutineContext[CoroutineName]} Received $y" }
                     delay(y*500L)
                     logger.info { "DONE with $y" }
                     logger.info { "----------------" }
