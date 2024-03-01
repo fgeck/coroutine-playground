eb43db01659811daa1b56a5f83efb5a65a20e159
Florian Geckeler <f.geckeler@sap.com>
2024-03-01 15:42:00 +0100

    improvements

--- app/src/main/kotlin/playground/channels/Channel.kt
+++ app/src/main/kotlin/playground/channels/Channel.kt
@@ -10,10 +10,16 @@ import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 
 class Channel {
+    private companion object {
+        private const val CONSUMERS_SIZE = 5
+        private const val CHANNEL_SIZE = 9
+        private const val NUMBERS = 20
+    }
+
     private val logger = KotlinLogging.logger {}
-    private val channel = Channel<Int>(5)
+    private val channel = Channel<Int>(CHANNEL_SIZE)
     private val consumers = mutableListOf<Job>()
-    private val someNumbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    private val someNumbers = (1..NUMBERS).toList()
 
     suspend fun startProducer(scope: CoroutineScope) {
         val producer = scope.launch {
@@ -27,13 +33,13 @@ class Channel {
     }
 
     fun startConsumer(scope: CoroutineScope) {
-        repeat(5) {
+        repeat(CONSUMERS_SIZE) {
             val consumer = scope.launch(CoroutineName("Consumer #${it + 1}")) {
-                delay(5000)
+                delay(500L)
                 for (y in channel) {
                     logger.info { "Coroutine: ${this.coroutineContext[CoroutineName]} Received $y" }
                     delay(y*500L)
-                    logger.info { "DONE with $y" }
+                    logger.info { "DONE with $y. Waited: ${y*500}ms" }
                     logger.info { "----------------" }
                 }
             }
@@ -44,6 +50,7 @@ class Channel {
     fun run() {
         runBlocking {
             startProducer(this)
+            // delay(1000L)
             startConsumer(this)
             consumers.forEach { it.join() }
         }
